6cc29c6429c377b7917318f0f081125913fefba8
msmubassir <125449220+msmubassir@users.noreply.github.com>
2024-06-25 12:08:18 +0600

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
 @app.route('/ip')
 def index():
     ip_address = request.remote_addr
-    return render_template('ip.html', ip=ip_address)
+    return render_template('ip.html', ip=ip_address, title='IP Address' )
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
