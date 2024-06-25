e16686536dff202ab35c3045b65e957d2dc65d78
msmubassir <125449220+msmubassir@users.noreply.github.com>
2024-06-25 12:28:26 +0600

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,29 @@ def about():
 
 @app.route('/ip')
 def show_ip():
-    # Check the 'X-Forwarded-For' header first
-    if request.headers.getlist("X-Forwarded-For"):
-        ip_address = request.headers.getlist("X-Forwarded-For")[0]
-    else:
-        ip_address = request.remote_addr
-    return render_template('ip.html', ip=ip_address, title='IP Address')
+    # Common headers used by proxies to pass client IP address
+    headers_to_check = [
+        "X-Forwarded-For",
+        "X-Real-IP",
+        "CF-Connecting-IP",  # Cloudflare
+        "True-Client-IP",    # Akamai and other CDNs
+    ]
 
+    # Loop through headers and get the first non-empty value
+    ip_address = None
+    for header in headers_to_check:
+        ip_address = request.headers.get(header)
+        if ip_address:
+            # If multiple IPs are present, take the first one
+            ip_address = ip_address.split(',')[0]
+            break
+    
+    # Fallback to request.remote_addr if no headers are found
+    if not ip_address:
+        ip_address = request.remote_addr
+    
+    return render_template('ip.html', ip=ip_address, title='IP Address')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
